b8114126bcd8d0d93b7992489a07e2a09386e3d5
hoanguyen81190 <hoanguyen81190@gmail.com>
2017-02-16 08:59:48 +0100

    Forgot to add the new component

---
+++ eal_stp/components/MDL/ConditionComponents.js
@@ -0,0 +1,146 @@
+import React from 'react';
+
+import cs from './ConditionComponents.css';
+
+import greenCheck from '../../resources/green_check.png';
+import redCheck from '../../resources/red_check.png';
+
+let uniqueID = 0;
+function generateID(){
+  uniqueID++;
+  return uniqueID;
+}
+
+export class Condition extends React.Component {
+  componentWillMount(){
+    this.id = generateID();
+  }
+  constructor() {
+    super();
+    this.type = "Leaf";
+    this.value = false;
+    this.children = [];
+  }
+
+  getValue() {
+    if (this.type === 'Leaf') {
+      return this.value;
+    }
+
+    var children = [];
+    this.children.map((item) => {children.push(this.refs[item].getValue())});
+
+    switch (this.type) {
+      case 'OR':
+        this.value = children.reduce((a, b) => a || b);
+        return this.value;
+      case 'AND':
+        this.value = children.reduce((a, b) => a && b);
+        return this.value;
+      case 'XOR':
+        this.value = children.reduce((a, b) => a ^ b);
+        return this.value;
+      default:
+        return false;
+    }
+  }
+
+  handleConditionClicked(){
+    this.value = !this.value;
+    if(this.props.callback !== undefined) {
+      this.props.callback();
+    }
+  }
+
+  renderLeafNode(){
+    var checkImage;
+
+    if(this.value){
+      checkImage = redCheck;
+    }
+    else{
+      checkImage = greenCheck;
+    }
+
+    var treeCardContent =
+    <div className={cs.leafContainer} onClick={this.handleConditionClicked.bind(this)}>
+      <div className={cs.conditionTextWrapper}>
+        <span dangerouslySetInnerHTML={{__html: this.props.content.description.text}}/>
+      </div>
+      <div className={cs.imageWrapper}>
+        <img className={cs.checkImage} src={checkImage} alt="Check icon"/>
+      </div>
+    </div>;
+
+    // var element =
+    //     <div>
+    //       {this.props.content.description.text}
+    //     </div>;
+
+    return treeCardContent;
+  }
+  renderLogicNode(){
+    var logicConditionColor;
+    if(this.value){
+     logicConditionColor = this.props.conditionColor.trueColor;
+    }
+    else{
+      logicConditionColor = this.props.conditionColor.falseColor;
+    }
+
+    // logicConditionColor = "mdl-color--indigo-50";
+
+    this.children = [];
+    var element = <table className={cs.conditionTable}>
+      <tbody>
+        {this.props.content.children.map((child, index) => {
+          var childRef = "child" + index;
+          this.children.push(childRef);
+          var borderBottomStyle = "";
+          if (index !== this.props.content.children.length - 1)
+          {
+            borderBottomStyle = cs.cellBorderBottom;
+          }
+
+          if(index === 0){
+            var row = <tr>
+              <td rowSpan={this.props.content.children.length}
+                className={cs.logicConditionText + " " + cs.conditionCell + " " + logicConditionColor}>
+                {this.props.content.type}
+              </td>
+
+              <td className={cs.conditionCell + " " + borderBottomStyle}>
+                <Condition ref={childRef} content={child} callback={this.props.callback} conditionColor={this.props.conditionColor}/>
+              </td>
+            </tr>
+          }
+          else {
+            var row = <tr>
+              <td className={cs.conditionCell + " " + borderBottomStyle}>
+                <Condition ref={childRef} content={child} callback={this.props.callback} conditionColor={this.props.conditionColor}/>
+              </td>
+            </tr>
+          }
+          return row;
+        })}
+      </tbody>
+    </table>;
+
+    return element;
+  }
+
+  render() {
+    var ele;
+    this.type = this.props.content.type;
+    if(this.type === undefined) {
+      ele = <div>Not Applicable</div>;
+    }
+    else if(this.type === "Leaf") {
+      ele = this.renderLeafNode();
+    }
+    else {
+      ele = this.renderLogicNode();
+    }
+    return ele;
+  }
+}
